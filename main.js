257f37eba2e3164e2c0793d21b4e26ba47be3d38
adrilla <amohidin@iamdigital.uk>
2017-02-09 17:02:51 +0000

    gang

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-var $form = $('#ticket-form');
-$form.on('submit', function(e) {
+var tt = {};
+
+tt.form= function($form) {
+	$form.on('submit', function(e) {
 	var values = {}, $inputs;
 
 	e.preventDefault();
@@ -13,6 +15,26 @@ $form.on('submit', function(e) {
 	});
 
 	window.localStorage.setItem('ticket', JSON.stringify(values));
+	});
+};
 
-});
+tt.list = function($list) {
+	var template = '<li class="ticket-list__item"><h4 class="ticket-list__title"> {{ name }}</h4><span class="ticket-list__user"> {{ title }}</span></li>';
+	var values = JSON.parse(window.localStorage.getItem('ticket'));
+
+	template = template.replace('{{ title }}', values.details);
+	template = template.replace('{{ name }}', values.name);
+
+	$list.html(template);
+};
+
+tt.main = function() {
+	var $form = $('#ticket-form');
+	var $list = $('#ticket-list');
+
+	tt.form($form);
+	tt.list($list);
+};
+
+	tt.main();
 
