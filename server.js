4828ff8c5d8c5341ccbe8ac1f6a8bf10fa417151
adrilla <amohidin@iamdigital.uk>
2017-02-09 11:55:09 +0000

    adrilla

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 
 var port = Number(argv.port) || Number(argv.p) || 3000;
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname));
 app.listen(port, function () {
   console.log('Quiztastic serving directory ' + __dirname + '/public on port ' + port);
 });
